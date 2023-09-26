00f9a8f32d9b2f9ae996bdd3e8dc903b9a831569
AdityaSingh-02 <asaditya2002@gmail.com>
2023-09-26 19:05:36 +0530

    update: btn component styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import {Second, CustomButton} from '@/components/index'
 export default function Home() {
   return(
     <>
-    <CustomButton style='blue' title='Hello world'/>
+    <CustomButton style='neon' title='Insert Text'/>
       {/* <Second /> */}
     </>
   )
--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -7,27 +7,27 @@ interface Props {
 }
 
 export const index = (props: Props) => {
-  const [btnColor, setBtnColor] = useState<string>("");
-  const [shadowColor, setShadowColor] = useState<string>("");
+  const [btnColor, setBtnColor] = useState<string>("3366FF");
+  const [shadowColor, setShadowColor] = useState<string>("3px 5px rgba(255, 255, 255, 1)");
 
   useEffect(() => {
     if (props.style == "blue") {
       setBtnColor("#3366FF");
-      setShadowColor("3xl")
+      setShadowColor("3px 5px rgba(255, 255, 255, 1)")
     } else if (props.style == "neon") {
       setBtnColor("#C6F806");
-      setShadowColor("4xl");
+      setShadowColor("3px 5px rgba(0, 0, 0, 1)");
     } else if (props.style == "black") {
       setBtnColor("#000000");
-      setShadowColor("3xl")
+      setShadowColor("3px 5px rgba(255, 255, 255, 1)")
     } else {
       setBtnColor("#FFFFFF");
-      setShadowColor("4xl")
+      setShadowColor("3px 5px rgba(0, 0, 0, 1)")
     }
-  }, []);
+  }, [props.style]);
 
   return (
-    <button className={`bg-[${btnColor}] px-5 py-2 rounded-full shadow-${shadowColor} font-semibold`}>
+    <button className={`px-5 py-2 rounded-full font-semibold`} style={{background:`${btnColor}` , boxShadow: `${shadowColor}`}}>
       {props.title}
     </button>
   );
