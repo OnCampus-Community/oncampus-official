35fbf37d0037e588838321beeb6a9f71428c170e
AdityaSingh-02 <asaditya2002@gmail.com>
2023-09-26 20:17:25 +0530

    add: Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Image from 'next/image'
-import {Second, CustomButton} from '@/components/index'
+import Image from "next/image";
+import { Navbar, Second, CustomButton } from "@/components/index";
 
 export default function Home() {
-  return(
+  return (
     <>
-    <CustomButton style='neon' title='Insert Text'/>
+      <div className="h-[100vh] w-screen bg-[#0C0634] flex justify-center">
+        <div className="pt-10">
+          <Navbar />
+        </div>
+      </div>
       {/* <Second /> */}
     </>
-  )
+  );
 }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,7 @@
-export {index as Second} from './Second/index'
+export { index as Second } from "./Second/index";
 
-export {index as Third} from './Third/index'
+export { index as Third } from "./Third/index";
 
-export {index as CustomButton} from './Buttons/index'  
+export { index as CustomButton } from "./Buttons/index";
+
+export { Navbar } from "./Navbar/Navbar";
