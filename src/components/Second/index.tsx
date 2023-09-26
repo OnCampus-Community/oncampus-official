bfb4396a9e535a9a93d4c68f4b9e2672857cf452
SahayakSharma <sahayaksharma6@gmail.com>
2023-09-26 15:04:49 +0530

    added css

--- src/components/Second/index.tsx
+++ src/components/Second/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import './second.css'
 
 export const index = () => {
   return (
-    <div>index</div>
+    <div className='second-container'>
+      <div className="second-wrapper"></div>
+    </div>
   )
 }
