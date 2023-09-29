c843f4b87f7080de92c5ae5856a79dcab919837b
SahayakSharma <sahayaksharma6@gmail.com>
2023-09-29 19:21:48 +0530

    modified projectcard and project page

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -11,15 +11,29 @@ interface Props {
 
 export const ProjectCard = (props: Props) => {
   return (
-    <div className="h-screen bg-gray-300">
+    <div className="h-[500px] bg-gray-300">
       <div
-        className="h-96 w-[90%] mx-auto rounded-2xl bg-[#3366FF] p-10 flex justify-center items-center"
+        className="h-96 w-[90%] mx-auto rounded-2xl bg-[white] p-10 flex justify-center items-center border-black border-solid border-2"
         style={{ boxShadow: "13px 15px black" }}>
-        <h2>{props.heading}</h2>
-        <Link href={props.url || " "}>
-          <Image src={props.img!} alt="image" height={100} width={100} />
-        </Link>
+
+         {/* BORDER COLOR NEEDS TO BE VARIFIED ------------------CHECK PLEASE------------------- */}
+
+
+          <div className="h-[100%] w-[70%] flex flex-col border-r-2 border-black text-black">
+            <div className="h-[30%] w-[100%] font-bold text-4xl px-10"><h2>{props.heading}</h2></div>
+            <div className="h-[70%] w-[100%] overflow-hidden px-10"><h2>{props.desc}</h2></div>
+          </div>
+          <div className="h-[100%] w-[30%]">
+          <Link href={props.url || " "}>
+              <Image src={props.img!} alt="image" height={350} width={350} className="m-auto"/>
+          </Link> 
+          </div>
       </div>
     </div>
   );
 };
+
+{/* <h2>{props.heading}</h2>
+<Link href={props.url || " "}>
+  <Image src={props.img!} alt="image" height={100} width={100} />
+</Link> */}
