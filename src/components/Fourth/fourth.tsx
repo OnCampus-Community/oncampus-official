92f7b5e049c9ba3d90e192ac4c6117a4ae97baa6
AdityaSingh-02 <asaditya2002@gmail.com>
2023-10-11 11:08:42 +0530

    chore: sliding cards

--- src/components/Fourth/fourth.tsx
+++ src/components/Fourth/fourth.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./fourth.module.css";
 import Image from "next/image";
-import { slide as Slide } from "../Slides/slide";
+import { slide as Slide, Props as IRevs } from "../Slides/slide";
+import revs from "./reviews.json";
 
 export const Fourth = () => {
+  const [reviews, setReviews] = useState<IRevs>({
+    im: "/person.png",
+    name: "John Doe",
+    position: "marketing",
+    text: "Hello world",
+  });
+
+  useEffect(() => {
+    setTimeout(() => {
+      setReviews(revs.rev1);
+    }, 3000);
+  }, []);
+
   return (
     <div className={style.fourth}>
       <div className={style.fourthwrapper}>
@@ -36,7 +50,12 @@ export const Fourth = () => {
           take our word for it – read what our clients have to say about their
           experience partnering with us.
         </div>
-        <Slide im="/person.png" name="John doe" position="marketing" text="Jello" />
+        <Slide
+          im={reviews.im}
+          name={reviews.name}
+          position={reviews.position}
+          text={reviews.text}
+        />
       </div>
     </div>
   );
